fbb59999568f664a705011e574614c00c798e8eb
daimps <daselj@ttu.ee>
2025-05-24 14:57:47 +0300

    PersonForm constructor

--- backend/src/main/java/com/helmesbackend/task/helmes/dto/PersonFormDTO.java
+++ backend/src/main/java/com/helmesbackend/task/helmes/dto/PersonFormDTO.java
@@ -4,13 +4,17 @@ import jakarta.validation.constraints.AssertTrue;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Set;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class PersonFormDTO {
     @NotBlank(message = "Name is required")
     private String name;
